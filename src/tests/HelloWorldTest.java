1aee569f34bfd3750675fdc2483e65feabcd4e59
xamsiye <xamsiye428@gmail.com>
2021-10-03 16:52:03 -0400

    updating  hello world test

--- src/tests/HelloWorldTest.java
+++ src/tests/HelloWorldTest.java
@@ -4,5 +4,8 @@ public class HelloWorldTest {
     public static void main(String[] args) {
         System.out.println("hello  world");
 
+
+        System.out.println("one more hello world");
+
     }
 }
